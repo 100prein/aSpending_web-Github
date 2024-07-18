24664f1d1dac634d8a039c83956d883f1b4408b3
Sekou Traore <37568350+100prein@users.noreply.github.com>
2024-07-18 14:10:30 -0400

    Notes progress

---
+++ src/scriptBtn.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const btnAdd = document.querySelector(".btn_add");
+
+btnAdd.addEventListener("click", () => addNote());
+
+function addNote(text = "") {
+    const note = document.createElement("div");
+    note.classList.add("note-wrapper");
+    note.innerHTML = `<div class="operations">
+                <button class="edit"><i class="fa-solid fa-pen-to-square" style="color: #63E6BE;"></i></button>
+                <button class="delete"><i class="fa-solid fa-trash" style="color: #63E6BE;"></i><i class="fa-solid fa-trash" style="color: #63E6BE;"></i></button>
+            </div>
+            <div class="main ${text ? "" : "hidden"}"></div>
+            <textarea class="${text ? "hidden" : ""}"></textarea>`;
+
+            const editBtn = note.querySelector(".edit");
+            const deleteBtn = note.querySelector(".delete");
+            const mainEl = note.querySelector(".main");
+            const textAreaEl = note.querySelector("textarea");
+
+            deleteBtn.addEventListener("click", () => {
+                note.remove();
+                updates();
+            });
+
+            editBtn.addEventListener("click", () => {
+                mainEl.classList.toggle("hidden");
+                textAreaEl.classList.toggle("hidden");
+            });
+
+            textAreaEl.addEventListener("input", (e) => {
+                const { value } = e.target;
+                mainEl.innerHTML = value;
+                updates();
+            });
+
+            document.body.appendChild(note);
+
+            function updates(){
+                const noteText = dcument.querySelectorAll('textArea');
+                const notes = [];
+
+                noteText.forEach((note) => notes.push(note.value));
+                localStorage.setItem("notes", JSON.stringify(notes));
+            }
+}
